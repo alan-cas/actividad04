cc76032bcc6fe92cf16190e821e295e23e75d8cb
alan_castañeda_ascencio <alan.castaneda5829@alumnos.udg.mx>
2020-09-21 13:58:13 -0500

    se agrego la funcion leer cadena

--- actividad2.c
+++ actividad2.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
+void leer_cadena(char cadena[20],int n){
+	for(int i=0;i<n;i++){
+		printf("%s\n",cadena);
+	}
+}
 void capturar_enteros(){
 	float suma=0;
 	int arreglo[5];
 	for (int i=0; i<5; i++){
 		printf("dame un entero: ");
-		scanf("%i",arreglo[i]);
+		scanf("%i",&arreglo[i]);
 		suma+=arreglo[i];
 	}
 	for (int i=0; i<5; i++){
@@ -16,14 +21,15 @@ void capturar_enteros(){
 }
 int main() {
     char opc;
-    
+    char cadena [20];
+    int n;
     do{
     	printf("1.- capturar 5 enteros");
     	printf("2.- leer 1 cadena e imprimirla n veces");
     	printf("3.- capturar personaje");
     	printf("4.- mostrar personaje");
     	printf("0.- salir");
-    	scanf("%c",opc);
+    	scanf("%c",&opc);
     	getchar();
     	
     	switch(opc){
@@ -31,6 +37,11 @@ int main() {
     			capturar_enteros();
     			break;
     		case '2':
+    			printf("dame una cadena: ");
+    			fgets(cadena,sizeof(cadena),stdin);
+    			printf("cuantas veces imprimiremos la cadena: ");
+    			scanf("%i",&n);
+    			leer_cadena(cadena,n);
     			break;
     		case '3':
     			break;
